0a590e93802bec53ee873aeef29739649f0682c5
Kyle Gildea <kylegildea@gmail.com>
2021-10-01 19:53:03 -0400

    fix

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -42,8 +42,7 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hello, again"
 
-	remoteIP := m.App.Session.GetStri
-	ng(r.Context(), "remote_ip")
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
 	stringMap["remote_ip"] = remoteIP
 
 	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
